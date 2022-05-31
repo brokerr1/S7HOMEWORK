5f4f96a0107f605e2e21a53a052a27d243b70c56
brokerr1 <solovievdima1@gmail.com>
2022-05-31 05:09:27 +0300

    1

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задайте двумерный массив размером M x N, заполненный случайными вещественными числами.
+*/
+int [,] GetArray(int a, int b)
+{
+    int [,] array = new int[a,b];
+    Random rndNum = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = rndNum.Next(10);
+        }     
+    } 
+    return array;
+}
+void PrintArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();     
+    } 
+}
+Console.WriteLine("Введите размерность массива");
+Console.Write("Количество строк:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Количество столбцов:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = GetArray(m, n);
+PrintArray(array);
